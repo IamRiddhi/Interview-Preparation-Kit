d3bfc774ace07a06aa64a4825546fa10f33c0c0d
Riddhi Dutta <rite2riddhi@gmail.com>
2019-11-06 23:30:48 +0530

    Update Next Greater Permutation.cpp

--- Arrays and Strings/Next Greater Permutation.cpp
+++ Arrays and Strings/Next Greater Permutation.cpp
@@ -1,10 +1,12 @@
-vector<int> Solution::nextPermutation(vector<int> &A) {
+#include <bits/stdc++.h>
+using namespace std;
+
+void next_permutation(vector<int> &A) {
     int size = A.size();
     for(int i=size-2;i>=0;i--)
     {
         if(A[i]<A[i+1])
         {
-            // sort(A.begin()+i+1,A.end());
             int f = i + 1;
             int l = size - 1;
             while(f < l)
@@ -19,18 +21,28 @@ vector<int> Solution::nextPermutation(vector<int> &A) {
             int t = A[i];
             A[i] = A[ind];
             A[ind] = t;
-            return A;
+            for(int j=0;j<size;j++)
+            	cout<<A[j];
+            cout<<endl;
+            return;
         }
     }
-        int f = 0;
-        int l = size - 1;
-        while(f < l)
-        {
-            int t = A[f];
-            A[f] = A[l];
-            A[l] = t;
-            f++;
-            l--;
-        }
-    return A;
+    cout<<"-1"<<endl;
+}
+
+int main() {
+	int t; cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<int> vc;
+		for(int i=0;i<n;i++)
+		{
+			int x; cin>>x; 
+			vc.push_back(x);
+		}
+		next_permutation(vc);
+	}
+	return 0;
 }
